b4d312101ee68266226550ecebde70238951ba66
Charles Freeborn <ceteure@yahoo.com>
2020-02-18 15:44:08 +0100

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const siteConfig = require('./config.js');
 const postCssPlugins = require('./postcss-config.js');
 
 module.exports = {
-  pathPrefix: "/charlesfreeborn.github.io",
+  pathPrefix: siteConfig.pathPrefix,
   siteMetadata: {
     url: siteConfig.url,
     title: siteConfig.title,
